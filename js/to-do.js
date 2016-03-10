2df0384667ae14b005810786c0574404dffb1c56
Jon Miranda <miranda.jra@gmail.com>
2016-03-09 17:09:22 -0800

    Refactor js and added dynamic quotes.

--- js/to-do.js
+++ js/to-do.js
@@ -1,76 +1,104 @@
 var delete_button = "<button type='button' class='delete_button'>X</button>";
 
-var loadList = function(id, list) {
-  if (list == undefined) {
-    list = []
-  }
+var loadList = function (id, list) {
+    if (list == undefined) {
+        list = []
+    }
 
-  for (var i = 0; i < list.length; ++i) {
-    $("#" + id + " ul").append("<li>" + delete_button  + "<input type='text' value='" + list[i] + "'/></li>");
-  }
-  return list;
+    for (var i = 0; i < list.length; ++i) {
+        $("#" + id + " ul").append("<li>" + delete_button + "<input type='text' value='" + list[i] + "'/></li>");
+    }
+    return list;
+};
+
+var loadQuote = function(quote) {
+    var quote = storage.quote;
+    var day = 24 * 60 * 60 * 1000;
+    var now = new Date();
+    if (!quote || (now - new Date(JSON.parse(quote['time'])) > day)) {
+        get_new_quote();
+    } else {
+        display_quote(quote['quote']);
+    }
 };
 
 // TODO: Sync between tabs chrome.storage.onChanged.addListener(function(changes, namespace).
-var loadItemsFromStorage = function() {
-  chrome.storage.local.get(["must_do", "tasks", "notes"], function(lists) {
-    loadList("must-do", lists.must_do);
-    loadList("tasks", lists.tasks);
-    loadList("notes", lists.notes);
-  });
-}
-
-var saveItems = function() {
-  console.log("Saving items...");
-
-  var must_do = [];
-  var tasks = [];
-  var notes = [];
-
-  var lists = [must_do, tasks, notes];
-  var ids = ["must-do", "tasks", "notes"];
-  for (var i = 0; i < ids.length; ++i) {
-    $("#" + ids[i] + " li input").each(function() {
-      lists[i].push($(this).val());
+var loadItemsFromStorage = function () {
+    chrome.storage.local.get(["must_do", "tasks", "notes", "quote"], function (storage) {
+        loadList("must-do", storage.must_do);
+        loadList("tasks", storage.tasks);
+        loadList("notes", storage.notes);
+        loadQuote(storage.quote);
     });
-  }
+};
 
-  chrome.storage.local.set(
-    {'must_do' : must_do, 'tasks' : tasks, 'notes' : notes}, 
-    function() {
-      if (chrome.runtime.lastError) {
-        console.log(chrome.runtime.lastError);
-      }
-  });
-}
+var saveItems = function () {
+    console.log("Saving items...");
 
-var deleteStorage = function () {
-  chrome.storage.local.clear();
-}
+    var must_do = [];
+    var tasks = [];
+    var notes = [];
 
-$(function() {
-  $( "#sortable1, #sortable2, #sortable3" ).sortable({
-    connectWith: ".connectedSortable",
-    receive: function(event, ui) {
-      saveItems();
+    var lists = [must_do, tasks, notes];
+    var ids = ["must-do", "tasks", "notes"];
+    for (var i = 0; i < ids.length; ++i) {
+        $("#" + ids[i] + " li input").each(function () {
+            lists[i].push($(this).val());
+        });
     }
-  }).disableSelection();
 
-  loadItemsFromStorage();
+    chrome.storage.local.set(
+        {'must_do': must_do, 'tasks': tasks, 'notes': notes},
+        function () {
+            if (chrome.runtime.lastError) {
+                console.log(chrome.runtime.lastError);
+            }
+        });
+};
 
-  $("body").on("change keyup paste", 'input', function() {
-    saveItems();
-  });
+var display_quote = function(quote) {
+    $("#quote").html(quote);
+}
 
-  $("body").on("click", '.delete_button', function() {
-    $(this).parent().remove();
-    saveItems();
-  });
-
-  $(".add-item").each(function() {
-    $(this).on("click", function() {
-      var parentId = $(this).data('parent-id');
-      $("#" + parentId + " ul").append("<li><input type='text'/>" + delete_button + " </li>");
+var get_new_quote = function() {
+    $.ajax({
+       url: "http://quotesondesign.com/api/3.0/api-3.0.json",
+        success: function(result) {
+            var quote = result['quote'];
+            display_quote(quote);
+            quote = {"quote": quote, "time": JSON.stringify(new Date())};
+            chrome.storage.local.set({"quote": quote});
+        }
     });
-  });
+};
+
+$(function () {
+    $("#sortable1, #sortable2, #sortable3").sortable({
+        connectWith: ".connectedSortable",
+        receive: function (event, ui) {
+            saveItems();
+        }
+    }).disableSelection();
+
+    loadItemsFromStorage();
+
+    $("body").on("change keyup paste", 'input', function () {
+        saveItems();
+    });
+
+    $("body").on("click", '.delete_button', function () {
+        $(this).parent().remove();
+        saveItems();
+    });
+
+    $("#quote").click(function() {
+        get_new_quote();
+    });
+
+    $(".add-item").each(function () {
+        $(this).on("click", function () {
+            var parentId = $(this).data('parent-id');
+            $("#" + parentId + " ul").append("<li><input type='text'/>" + delete_button + " </li>");
+        });
+    });
 });
